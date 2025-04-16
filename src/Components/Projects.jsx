565a1f53e4bcd1786ee6fe89bd727b85a3744682
Pragya Mittal <69578414+pragyamittal0@users.noreply.github.com>
2025-04-15 19:40:27 -0500

    Update Projects.jsx

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -10,30 +10,35 @@ const Project = () => {
       id: 1,
       name: "Personalized Healthcare Treatment Using RL",
       image: "/image/p1.png",
+      detail: "Designed an RL-based system using Q-learning and Deep Q-Networks (DQN) to personalize treatment plans, optimizing patient outcomes dynamically.  🚀",
       navigateTo: "/project-details/1",
     },
     {
       id: 2,
       name: "FashionMNIST Classifier",
       image: "/image/p2.png",
+      detail:"Compared KNN, SVM, and decision trees with CNNs for fashion image classification. Preprocessed data with standardization and PCA, optimizing models for accuracy and efficiency. CNNs dominated with 91.98% accuracy, while RBF SVM and KNN provided strong alternatives with lower computational costs—offering key insights for real-world applications like retail automation and quality control. 🚀",
       navigateTo: "/project-details/2",
     },
     {
       id: 3,
       name: "Sentimental Analysis",
       image: "/image/p3.png",
+      detail:"Built a sentiment analysis tool leveraging BERT to classify text into positive, negative, or neutral sentiments. The model provides high accuracy in understanding contextual sentiment from customer reviews and social media posts.",
       navigateTo: "/project-details/3",
     },
     {
         id: 4,
         name: "Oxywin",
         image: "/image/p4.png",
+        detail:"A web-based platform developed during COVID-19 to streamline hospital oxygen logistics. It enables real-time tracking, order placement, and coordination between hospitals, suppliers, and transporters—ensuring efficient distribution and preventing shortages. 🚑🔗",
         navigateTo: "/project-details/4",
     },
     {
         id: 5,
         name: "Personalized Learning Toolkit",
         image: "/image/p5.png",
+        detail:"An AI-driven system that curates tailored learning resources based on users’ knowledge levels and goals. It recommends courses, books, and videos from platforms like Coursera and Udemy, making self-paced learning more structured and engaging. 🚀",
         navigateTo: "/project-details/5",
     },
   ];
@@ -57,6 +62,7 @@ const Project = () => {
           >
             <img src={project.image} alt={project.name} className="project-image" />
             <h3 className="project-name">{project.name}</h3>
+            <p className="project-detail">{project.detail}</p>
           </div>
         ))}
       </div>
