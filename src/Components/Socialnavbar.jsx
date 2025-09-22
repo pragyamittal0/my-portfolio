882c7468ff82a5334fc4ce4ab5308509ec47f6f8
Pragya Mittal <69578414+pragyamittal0@users.noreply.github.com>
2025-09-22 16:09:53 -0700

    Update Socialnavbar.jsx

--- src/Components/Socialnavbar.jsx
+++ src/Components/Socialnavbar.jsx
@@ -12,34 +12,7 @@ const SocialNavbar = () => {
       <a href="https://github.com/pragyamittal0" target="_blank" rel="noopener noreferrer">
         <FaGithub className="social-icon" />
       </a>
-      <a href="import React from "react";
-import "./SocialNav.css";
-import { FaLinkedin, FaGithub, FaFilePdf, FaHeart } from "react-icons/fa";
-
-const SocialNavbar = () => {
-  return (
-    <div className="full-navbar">
-    <div className="social-navbar">
-      <a href="https://www.linkedin.com/in/pragya-mittal-7a858b1b1/" target="_blank" rel="noopener noreferrer">
-        <FaLinkedin className="social-icon" />
-      </a>
-      <a href="https://github.com/pragyamittal0" target="_blank" rel="noopener noreferrer">
-        <FaGithub className="social-icon" />
-      </a>
       <a href="https://drive.google.com/file/d/1_yA1ggl861vwDoxhoitX70JiNCkRxUmA/view?usp=sharing" download target="_blank" rel="noopener noreferrer">
-        <FaFilePdf className="social-icon" />
-      </a>
-      </div>
-
-      <div className="footer-message">
-        <FaHeart className="heart-icon" />  Pragya
-      </div>
-    </div>
-  );
-};
-
-export default SocialNavbar;
-" download target="_blank" rel="noopener noreferrer">
         <FaFilePdf className="social-icon" />
       </a>
       </div>
