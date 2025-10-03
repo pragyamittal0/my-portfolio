ca723526dde188e5da0dbdc40dc7f41bbfbbe76c
Pragya Mittal <69578414+pragyamittal0@users.noreply.github.com>
2025-10-03 16:34:41 -0500

    Update Socialnavbar.jsx

--- src/Components/Socialnavbar.jsx
+++ src/Components/Socialnavbar.jsx
@@ -12,7 +12,7 @@ const SocialNavbar = () => {
       <a href="https://github.com/pragyamittal0" target="_blank" rel="noopener noreferrer">
         <FaGithub className="social-icon" />
       </a>
-      <a href="https://drive.google.com/file/d/1agyFXsfus8Vcxi_gTkGPJ_L1g7x1-1N-/view?usp=sharing" download target="_blank" rel="noopener noreferrer">
+      <a href="https://drive.google.com/file/d/1MJ574faiEu4FHdbBglrKWNmgkKtWUmnI/view?usp=sharing" download target="_blank" rel="noopener noreferrer">
         <FaFilePdf className="social-icon" />
       </a>
       </div>
