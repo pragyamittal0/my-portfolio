aa42e24a0f3227c17c9ab39b3899caa5d55e4bd4
Pragya Mittal <69578414+pragyamittal0@users.noreply.github.com>
2025-10-18 20:45:31 -0500

    Modify Experience section with updated title and description
    Updated job title and enhanced job description with additional details about responsibilities and achievements.

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -4,11 +4,11 @@ import "./Experience.css"; // Importing the CSS file
 const Experience = () => {
   const ExperienceData = [
     {
-      title: "Machine Learning Engineer",
+      title: "Machine Learning Engineer/ data scientist/ data engineer",
       duration: "September, 2025 - Present",
       institution: "Emotionall",
       description:
-        "Developing a Gamification Progress Tracker algorithm for a mental wellness app to drive habit formation—designing XP systems,streak-based rewards, milestone tracking, and emotional insight reports to improve engagement and retention. Led integration of Admin Panel, MixPanel, RevenueCat, and OneSignal, defining scalable data pipelines, secure APIs, and cross-tool syncing architecture—achieved 99.9% data reliability and reduced debugging time by 30%.",
+        "Led integration of Admin Panel, MixPanel, RevenueCat, and OneSignal, defining scalable data pipelines, secure APIs, and cross-tool syncing architecture—achieved 99.9% data reliability and reduced debugging time by 30%.Developing a Gamification Progress Tracker algorithm for a mental wellness app to drive habit formation—designing XP systems,streak-based rewards, milestone tracking, and emotional insight reports to improve engagement and retention. Performed data scraping of oil \& gas firms and unit testing of frontend events, ensuring analytics accuracy and schema consistency.",
     },
     {
       title: "Software Engineer - GenAI",
